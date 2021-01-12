8605228beea297fbe13e85e5c7e0c7f81880e6b1
alenj <alenjojo252001@gmail.com>
2021-01-12 20:08:08 +0530

    winner

--- main.py
+++ main.py
@@ -51,13 +51,13 @@ def check_if_win():
 
     if row_winner:
         # there is a win
-        winner = row_winner()
+        winner = row_winner
     elif col_winner:
         # there is a win
-        winner = col_winner()
+        winner = col_winner
     elif diagonal_winner:
         # there is a win
-        winner = diagonal_winner()
+        winner = diagonal_winner
     else:
         # there is no win
         winner = None
@@ -65,15 +65,56 @@ def check_if_win():
 
 
 def check_rows():
+
+    global game_on
+
     row_1 = board[0] == board[1] == board[2] != "-"
+    row_2 = board[3] == board[4] == board[5] != "-"
+    row_3 = board[6] == board[7] == board[8] != "-"
+
+    if row_1 or row_2 or row_3:
+        game_on = False
+    if row_1:
+        return board[0]
+    elif row_2:
+        return board[3]
+    elif row_3:
+        return board[6]
     return
 
 
 def check_col():
+
+    global game_on
+
+    col_1 = board[0] == board[3] == board[6] != "-"
+    col_2 = board[1] == board[4] == board[7] != "-"
+    col_3 = board[2] == board[5] == board[8] != "-"
+
+    if col_1 or col_2 or col_3:
+        game_on = False
+    if col_1:
+        return board[0]
+    elif col_2:
+        return board[1]
+    elif col_3:
+        return board[2]
     return
 
 
 def check_diagonals():
+
+    global game_on
+
+    diagonal_1 = board[0] == board[4] == board[8] != "-"
+    diagonal_2 = board[6] == board[4] == board[2] != "-"
+
+    if diagonal_1 or diagonal_2:
+        game_on = False
+    if diagonal_1:
+        return board[0]
+    elif diagonal_2:
+        return board[6]
     return
 
 
